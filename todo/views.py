0a74a2f0eab4ec2b0b50d70ce3e4fe6c8113165f
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-07-23 14:22:20 +0100

    views.py modified post deployment

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics, T
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer, RegisterSerializer
