a09a4f51f0323c4849d1f2c0652d674c98741339
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-07-23 07:15:42 +0100

    final commit - Task Management API

---
+++ todo/urls.py
@@ -0,0 +1,17 @@
+from rest_framework import routers
+from .views import TaskViewSet, RegisterView
+from django.urls import path
+
+
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet, basename='tasks')
+
+urlpatterns = router.urls + [
+      
+
+path('register/', RegisterView.as_view(), name='register'),         
+
+]
+
+          
+      
---
+++ todo/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer, RegisterSerializer
+from django.db.models import Q
+from .permissions import IsOwnerOrReadOnly
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+       tasks= Task.objects.filter(user=self.request.user)
+       completed_param = self.request.GET.get('completed')
+       search_param = self.request.GET.get("search")
+       
+       if  completed_param is not None:
+           if completed_param.lower() == 'true':
+               tasks = tasks.filter(completed= True)
+           elif completed_param.lower() == 'false':
+               tasks = tasks.filter(completed= False)    
+               
+       if search_param:
+           tasks = tasks.filter(
+               Q(title__icontains=search_param) |
+               Q(description__icontains=search_param)
+               ) 
+       return tasks         
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = []
+
+    
