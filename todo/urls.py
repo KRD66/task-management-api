93fd5860cc08cbf56d0d156fee6844fb257dd0b0
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-07-23 18:44:55 +0100

    loginview created

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
 from .router import urlpatterns as router_urls
+from .views import LoginView
 
 urlpatterns = [
     path('', include(router_urls)), 
-    path('api/', include(router_urls)),  
+    path('api/', include(router_urls)),
+    path('login/', LoginView.as_view(), name='login'),  
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer, RegisterSerializer
 from django.db.models import Q
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
 
 
@@ -38,4 +42,19 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = []
 
-    
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+         token, created = Token.objects.get_or_create(user=user)
+         return Response({"token":token.key})
+        else:
+         return Response({"error":"Invalid credentials"}, status=400)
+     
+     
+     
+     
+     
+     
